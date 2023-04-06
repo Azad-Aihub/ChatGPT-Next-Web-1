b7d774f3136c283d04ee882317a4b3669354a1c1
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2023-04-06 15:25:22 +0800

    Add files via upload

---
+++ app/constant.ts
@@ -0,0 +1,5 @@
+export const OWNER = "buy";
+export const REPO = "42";
+export const REPO_URL = `https://faka.azad.asia/${OWNER}/${REPO}`;
+export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/Yidadaa/ChatGPT-Next-Web/commits?per_page=1`;
