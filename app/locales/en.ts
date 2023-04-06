d3e8d0d08cd32f4d1c552fa3194a3312b9bdf4b2
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2023-04-06 15:26:27 +0800

    Add files via upload

---
+++ app/locales/en.ts
@@ -0,0 +1,124 @@
+import type { LocaleType } from "./index";
+
+const en: LocaleType = {
+  WIP: "WIP...",
+  Error: {
+    Unauthorized:
+      "Unauthorized access, please enter access code in settings page.",
+  },
+  ChatItem: {
+    ChatItemCount: (count: number) => `${count} messages`,
+  },
+  Chat: {
+    SubTitle: (count: number) => `${count} messages with ChatGPT`,
+    Actions: {
+      ChatList: "Go To Chat List",
+      CompressedHistory: "Compressed History Memory Prompt",
+      Export: "Export All Messages as Markdown",
+      Copy: "Copy",
+      Stop: "Stop",
+      Retry: "Retry",
+    },
+    Typing: "Typing…",
+    Input: (submitKey: string) =>
+      `Type something and press ${submitKey} to send`,
+    Send: "Send",
+  },
+  Export: {
+    Title: "All Messages",
+    Copy: "Copy All",
+    Download: "Download",
+  },
+  Memory: {
+    Title: "Memory Prompt",
+    EmptyContent: "Nothing yet.",
+    Copy: "Copy All",
+  },
+  Home: {
+    NewChat: "New Chat",
+    DeleteChat: "Confirm to delete the selected conversation?",
+  },
+  Settings: {
+    Title: "Settings",
+    SubTitle: "All Settings",
+    Actions: {
+      ClearAll: "Clear All Data",
+      ResetAll: "Reset All Settings",
+      Close: "Close",
+    },
+    Lang: {
+      Name: "语言",
+      Options: {
+        cn: "中文",
+        en: "English",
+      },
+    },
+    Avatar: "Avatar",
+    Update: {
+      Version: (x: string) => `Version: ${x}`,
+      IsLatest: "Latest version",
+     
+    },
+    SendKey: "Send Key",
+    Theme: "Theme",
+    TightBorder: "Tight Border",
+    HistoryCount: {
+      Title: "Attached Messages Count",
+      SubTitle: "Number of sent messages attached per request",
+    },
+    CompressThreshold: {
+      Title: "History Compression Threshold",
+      SubTitle:
+        "Will compress if uncompressed messages length exceeds the value",
+    },
+    Token: {
+      Title: "API Key",
+      SubTitle: "Use your key to ignore access code limit. Click on the lower left corner to buy.",
+      Placeholder: "OpenAI API Key",
+    },
+    AccessCode: {
+      Title: "Access Code",
+      SubTitle: "Join QQ group 340825795 to get the access code.",
+      Placeholder: "Need Access Code",
+    },
+    Model: "Model",
+    Temperature: {
+      Title: "Temperature",
+      SubTitle: "A larger value makes the more random output",
+    },
+    MaxTokens: {
+      Title: "Max Tokens",
+      SubTitle: "Maximum length of input tokens and generated tokens",
+    },
+    PresencePenlty: {
+      Title: "Presence Penalty",
+      SubTitle:
+        "A larger value increases the likelihood to talk about new topics",
+    },
+    Zhishixingqiu: {
+      Title: "点击加入<a href="https://t.zsxq.com/0cy1WzxXu" target="_blank">知识星球</a>",
+      SubTitle: "获取ChatGPT独享账号，免梯子使用星主搭建并持续更新的ChatGPT项目以及其他福利",
+    },
+  },
+  Store: {
+    DefaultTopic: "New Conversation",
+    BotHello: "Hello! How can I assist you today?",
+    Error: "Something went wrong, please try again later.",
+    Prompt: {
+      History: (content: string) =>
+        "This is a summary of the chat history between the AI and the user as a recap: " +
+        content,
+      Topic:
+        "Provide a brief topic of the sentence without explanation. If there is no topic, return 'Chitchat'.",
+      Summarize:
+        "Summarize our discussion briefly in 50 characters or less to use as a prompt for future context.",
+    },
+    ConfirmClearAll: "Confirm to clear all chat and setting data?",
+  },
+  Copy: {
+    Success: "Copied to clipboard",
+    Failed: "Copy failed, please grant permission to access clipboard",
+  },
+};
+
+export default en;
