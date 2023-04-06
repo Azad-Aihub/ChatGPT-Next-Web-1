0600b8691becdc0866d19735d5ca4e956d5c0992
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2023-04-06 17:03:44 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "buy";
 export const REPO = "42";
 export const REPO_URL = `https://faka.azad.asia/${OWNER}/${REPO}`;
-export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const UPDATE_URL = `https://t.zsxq.com/0cy1WzxXu`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/Yidadaa/ChatGPT-Next-Web/commits?per_page=1`;
